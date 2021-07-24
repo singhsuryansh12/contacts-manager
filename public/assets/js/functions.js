09e346bb847e889237ccd54148a221253ecfbedc
singhsuryansh12 <nishusinghjbp@gmail.com>
2021-07-24 17:45:38 +0530

    Added data write functions

---
+++ public/assets/js/functions.js
@@ -0,0 +1,17 @@
+let firstName = document.getElementById('firstName');
+let lastName = document.getElementById('lastName');
+let email = document.getElementById('email');
+let addButton = document.getElementById('addButton');
+
+const database = firebase.database();
+let count = 0;
+
+addButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    database.ref('/users/' + count).set({
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+    })
+    count++;
+})
